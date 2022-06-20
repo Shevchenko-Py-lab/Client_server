f0f23321b86762ac329a9371c39a187a7f0e8ade
Shevchenko-Py-lab <shevchenko.logist@mail.ru>
2022-06-20 22:09:01 +0300

    Test_task_deco

--- Task_3/task_3_server.py
+++ Task_3/task_3_server.py
@@ -9,10 +9,12 @@ from errors import IncorrectDataReceivedError
 
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
 from common.utils import get_message, send_message
+from log_decorator import log
 
-SERVER_LOGGER = logging.getLogger('server')
+LOGGER = logging.getLogger('server')
 
 
+@log
 def process_client_message(message):
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and message[USER][ACCOUNT_NAME] == 'LoggedUser':
@@ -23,6 +25,7 @@ def process_client_message(message):
     }
 
 
+@log
 def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
@@ -30,16 +33,17 @@ def create_arg_parser():
     return parser
 
 
+@log
 def main():
     parser = create_arg_parser()
     namespace = parser.parse_args(sys.argv[1:])
     listen_address = namespace.a
     listen_port = namespace.p
     if not 1023 < listen_port < 65536:
-        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
+        LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
                                f'{listen_port}. Допустимы адреса с 1024 до 65535.')
         sys.exit(1)
-    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+    LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
                        f'адрес с которого принимаются подключения: {listen_address}. '
                        f'Если адрес не указан, принимаются соединения с любых адресов.')
 
@@ -50,21 +54,21 @@ def main():
 
     while True:
         client_sock, client_address = serv_sock.accept()
-        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
+        LOGGER.info(f'Установлено соедение с ПК {client_address}')
         try:
             message_from_client = get_message(client_sock)
-            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
+            LOGGER.debug(f'Получено сообщение {message_from_client}')
             response = process_client_message(message_from_client)
-            SERVER_LOGGER.info(f'Сформирован ответ клиенту {response}')
+            LOGGER.info(f'Сформирован ответ клиенту {response}')
             send_message(client_sock, response)
-            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
             client_sock.close()
         except json.JSONDecodeError:
-            SERVER_LOGGER.error(f'Не удалось декодировать Json строку, полученную от '
+            LOGGER.error(f'Не удалось декодировать Json строку, полученную от '
                                 f'клиента {client_address}. Соединение закрывается.')
             client_sock.close()
         except IncorrectDataReceivedError:
-            SERVER_LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
+            LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
                                 f'Соединение закрывается.')
             client_sock.close()
 
