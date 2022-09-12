629e9ddaa9fbcc3084b015b34b6c864f4617b50d
Shevchenko-Py-lab <shevchenko.logist@mail.ru>
2022-09-12 16:50:07 +0300

    PyQT_OOP_Task_7

---
+++ Project_to_OOP/doc/source/Project_to_OOP.log_project.rst
@@ -0,0 +1,29 @@
+Project\_to\_OOP.log\_project package
+=====================================
+
+Submodules
+----------
+
+Project\_to\_OOP.log\_project.config\_client\_log module
+--------------------------------------------------------
+
+.. automodule:: Project_to_OOP.log_project.config_client_log
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Project\_to\_OOP.log\_project.config\_server\_log module
+--------------------------------------------------------
+
+.. automodule:: Project_to_OOP.log_project.config_server_log
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: Project_to_OOP.log_project
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- Project_to_OOP/unit_tests/log_decorator.py
+++ Project_to_OOP/unit_tests/log_decorator.py
@@ -1,22 +1,33 @@
 import sys
 import logging
+import socket
 import Project_to_OOP.log_project.config_server_log
 import Project_to_OOP.log_project.config_client_log
 import traceback
 import inspect
 
+sys.path.append('../')
 # метод определения модуля, источника запуска.
 if sys.argv[0].find('client_dist') == -1:
-    LOGGER = logging.getLogger('server_dist')
+    logger = logging.getLogger('server_dist')
 else:
     # ну, раз не сервер, то клиент
-    LOGGER = logging.getLogger('client_dist')
+    logger = logging.getLogger('client_dist')
 
 
 def log(func_to_log):
+    """
+    Декоратор, выполняющий логирование вызовов функций.
+    Сохраняет события типа debug, содержащие
+    информацию о имени вызываемой функиции, параметры с которыми
+    вызывается функция, и модуль, вызывающий функцию.
+    """
+
     def log_saver(*args, **kwargs):
-        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args} , {kwargs}. '
-                     f'Вызов из модуля {func_to_log.__module__}')
+        logger.debug(
+            f'Была вызвана функция {func_to_log.__name__} c параметрами {args} , {kwargs}. '
+            f'Вызов из модуля {func_to_log.__module__}')
         ret = func_to_log(*args, **kwargs)
         return ret
+
     return log_saver
